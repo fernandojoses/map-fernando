75e48c6813d4067b8865c1263d5f34bb0de79b32
Fernando Sagastume Sagastume <fjsaga60>
2020-03-06 11:05:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
 pygame.init()
 
-win = pygame.display.set_mode((800, 600))
+win = pygame.display.set_mode((800, 615))
 img1 = pygame.image.load('img1.png')
+img2 = pygame.image.load('img2.png')
+img3 = pygame.image.load('img3.png')
 
 run = True
 while run:
@@ -15,7 +17,23 @@ while run:
         
         for j in range(50):
          win.blit(img1, (i * 47, j * 47))
+    
+    row = [1, 0, 0, 1]
 
+    for i in range(4):
+        if row[i] == 1:
+            win.blit(img2, (47, 0))
+        if row[i] == 0:
+            win.blit(img2, (i, 0))
+            
+
+    row = [1, 0, 0, 1]
+
+    for i in range(4):
+        if row[i] == 1:
+            win.blit(img3, (90, 0))
+        if row[i] == 0:
+            win.blit(img3, (i, 0))
 
     pygame.display.update()
 
